e94b0886885c105fe0b3bb625cdbb1f1b807f80c
kylian-harmsen <D296238@edu.curio.nl>
2025-09-11 16:24:37 +0200

    added shack area

--- text based adventure/Program.cs
+++ text based adventure/Program.cs
@@ -2,6 +2,7 @@
 Boolean shack = false;
 Boolean forest = false;
 Boolean door = false;
+Boolean bastment = false;
 Boolean GameOn = true;
 Boolean lock1 = false;
 Boolean lock2 = false;
@@ -9,6 +10,10 @@ Boolean lock3 = false;
 Boolean smallKey = false;
 Boolean mediumKey = false;
 Boolean bigKey = false;
+Boolean shovel = false;
+Boolean code = false;
+Boolean bookshelf = false;
+Boolean book = false;
 
 Console.WriteLine("Welcome to text based adventure\n\nDo you want to start y/n\n");
 string Awnser = Console.ReadLine();
@@ -56,7 +61,67 @@ else if (Awnser == "y" || Awnser == "Y")
         }
         while (shack == true)
         {
+            if (bookshelf == false)
+            {
+                Console.WriteLine("your indside the old abandoned shack you see a strangly placed bookshelf a safe and a rack of tools\n");
+                Console.WriteLine("what do you wannt to ivestigate shelf safe tools or just leave\n");
+            }
+            else if (bookshelf == true)
+            {
+                Console.WriteLine("your indside the old abandoned shack you see the stairs leading down to a basement a safe and a rack of tools\n");
+                Console.WriteLine("what do you wannt to ivestigate stairs safe tools or just leave\n");
+            }
+            Awnser = Console.ReadLine();
+            Console.WriteLine();
+            if (Awnser == "shelf" && bookshelf == false && book == false)
+            {
+                Console.WriteLine("looking at the bookshelf you just cant shake the feeling its in such an od place and looking closer the entire thing still seems to be full of books exept for one place maby you could place something inside\n");
+            }
+            else if (Awnser == "shelf" && bookshelf == false && book == true)
+            {
+                Console.WriteLine("you place the book inside and just like that the closet starts moving on its own revealing a staircase to a bastment\n");
+                bookshelf = true;
+            }
+            else if (Awnser == "tools" && shovel == false)
+            {
+                Console.WriteLine("you see a bunch of old worn out tools the shovel seems in good shape so you take it could be handy later\n");
+                shovel = true;
+            }
+            else if (Awnser == "tools" && shovel == true)
+            {
+                Console.WriteLine("all the other tools either look to worn out or useless\n");
+            }
+            else if (Awnser == "safe" && code == false && book == false)
+            {
+                Console.WriteLine("a safe with a 5 didgit code on it you wonder whats inside but its no use guesing the combination\n");
+            }
+            else if (Awnser == "safe" && code == true && book == false)
+            {
+                Console.WriteLine("you open the safe and find a normal sized key and a book inside\n");
+                mediumKey = true;
+                book = true;
+            }
+            else if (Awnser == "safe" && code == true && book == true)
+            {
+                Console.WriteLine("just an empty safe now\n");
+            }
+            else if (Awnser == "stairs" && bookshelf == true)
+            {
+                Console.WriteLine("you go down the stairs and enter the bastment\n");
+                bastment = true;
+            }
+            else if (Awnser == "leave" && Awnser == "just leave")
+            {
+                shack = false;
+            }
+            else
+            {
+                Console.WriteLine("not a valid awnser");
+            }
+            while (bastment == true)
+            {
 
+            }
         }
         while (forest == true)
         {
@@ -92,7 +157,7 @@ else if (Awnser == "y" || Awnser == "Y")
             }
             else if (Awnser == "lock1" && lock1 == true)
             {
-                Console.WriteLine("you thin to yourself that you already did this lock\n");
+                Console.WriteLine("you think to yourself that you already did this lock\n");
             }
             else if (Awnser == "lock2" && lock2 == false)
             {
@@ -111,7 +176,7 @@ else if (Awnser == "y" || Awnser == "Y")
             }
             else if (Awnser == "lock2" && lock2 == true)
             {
-                Console.WriteLine("you thin to yourself that you already did this lock\n");
+                Console.WriteLine("you think to yourself that you already did this lock\n");
             }
             else if (Awnser == "lock3" && lock3 == false)
             {
@@ -128,9 +193,9 @@ else if (Awnser == "y" || Awnser == "Y")
                     lock3 = true;
                 }
             }
-            else if (Awnser == "lock2" && lock3 == true)
+            else if (Awnser == "lock3" && lock3 == true)
             {
-                Console.WriteLine("you thin to yourself that you already did this lock\n");
+                Console.WriteLine("you think to yourself that you already did this lock\n");
             }
             else if (Awnser == "leave" && Awnser == "just leave")
             {
